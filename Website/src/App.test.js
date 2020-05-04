3ae9fa0bde0f03ca1079cb79f3814c5bbac359d9
cosund <cosu@ktth.se>
2020-05-04 11:42:47 +0200

    Test v.2

--- Website/src/App.test.js
+++ Website/src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Object } from './../config/env';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
